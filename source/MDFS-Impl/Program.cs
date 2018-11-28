aca699d017a9f9632636a19bea570a54951b435b
Arawn Davies <arawn.davies780@gmail.com>
2018-11-28 16:06:08 +0000

    Add implementation project

---
+++ source/MDFS-Impl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MDFS_Impl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
