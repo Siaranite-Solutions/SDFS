90801fcf346324f888b0c6b576f5b5bb07dc44d5
Arawn-Davies <arawn.davies@gmail.com>
2018-12-02 16:40:08 +0000

    Initial class definition of a Master Boot Record (MBR)

--- source/SDFS/Physical/MBR.cs
+++ source/SDFS/Physical/MBR.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Cosmos.HAL.BlockDevice;
 
 namespace SDFS.Physical
 {
     public class MBR
     {
+        /// <summary>
+        /// The block device on which this MBR resides
+        /// </summary>
+        private BlockDevice mBlockDevice;
+
+        /// <summary>
+        /// Constructor for a Master Boot Record
+        /// </summary>
+        /// <param name="mbr"></param>
+        /// <param name="aBlockDevice"></param>
+        public MBR (Byte[] mbr, BlockDevice aBlockDevice)
+        {
+            this.mBlockDevice = aBlockDevice;
+        }
+
+        /// <summary>
+		/// The Byte Array containing the bootable code and partition information of the MBR
+		/// </summary>
+		public readonly Byte[] Bootable = new Byte[440];
+
+        /// <summary>
+		/// The Disk signature
+		/// </summary>
+		public readonly UInt32 Signature = 0;
+
+
     }
 }
