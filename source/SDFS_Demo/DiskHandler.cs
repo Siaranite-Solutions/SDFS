bc6833a677c7a7f236c9408d6c8057825e40f902
Arawn-Davies <arawn.davies@gmail.com>
2018-12-03 00:21:01 +0000

    Added partitioning methods

---
+++ source/SDFS_Demo/DiskHandler.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cosmos.HAL.BlockDevice;
+using SDFS.Physical;
+
+namespace SDFS_Demo
+{
+    class DiskHandler
+    {
+        /// <summary>
+        /// Partitioning function for devices that present a choise of the device to partition.
+        /// </summary>
+        /// <param name="list">The list of the devices to choose from</param>
+        public static void CreatePartitions(SDFS.Physical.IDE[] list)
+        {
+            SDFS.Physical.IDE Device = null;
+            int partnum = 0;
+            ulong DispCount = 0;
+            Console.WriteLine("SDFS Partitioning Tool");
+            do
+            {
+                Console.WriteLine("Which device do you want to use?");
+                for (int i = 0; i < list.Length; i++)
+                {
+                    Console.WriteLine(" --- Device N." + (i + 1) + " Size: approximately " + (uint)((((list[i].BlockSize * list[i].BlockCount) / 1024) / 1024) + 1) + " MB");
+                }
+                Console.Write("Insert Number: ");
+                String nums = Console.ReadLine();
+                int num = int.Parse(nums) - 1;
+                if (num >= 0 && num < list.Length)
+                {
+                    Device = list[num];
+                    DispCount = list[num].BlockCount - 1;
+                }
+            } while (Device == null);
+            Console.WriteLine("How many primary partitions? (Max. 4)");
+            do
+            {
+                Console.Write("Insert Number: ");
+                String nums = Console.ReadLine();
+                partnum = int.Parse(nums);
+            } while (partnum == 0 || partnum > 4);
+            uint mbrpos = 446;
+            Byte[] type = new Byte[] { 0x00, 0x00, 0x00, 0x00 };
+            uint[] StartBlock = new uint[] { 1, 0, 0, 0 };
+            uint[] BlockNum = new uint[] { 0, 0, 0, 0 };
+            for (int i = 0; i < partnum; i++)
+            {
+                type[i] = 0xFA;
+                Console.Write("How many blocks for Partition N. " + (i + 1) + "? (Max: " + ((uint)(DispCount - (uint)(partnum - (i + 1)))).ToString() + "): ");
+                String nums = Console.ReadLine();
+                uint num = (uint)int.Parse(nums);
+                if (num >= 0 && num <= DispCount - (uint)(partnum - (i + 1)))
+                {
+                    BlockNum[i] = num;
+                    if (i < partnum - 1)
+                    {
+                        StartBlock[i + 1] = (num) + StartBlock[i];
+                    }
+                    DispCount = DispCount - (num);
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            Byte[] data = Device.NewBlockArray(1);
+            Device.ReadBlock(0, 1, data);
+            for (int i = 0; i < 4; i++)
+            {
+                mbrpos += 4;
+                data[mbrpos] = type[i];
+                mbrpos += 4;
+                Byte[] b = BitConverter.GetBytes(StartBlock[i]);
+                SDFS.UtilityMethods.CopyByteToByte(b, 0, data, (int)mbrpos, b.Length);
+                mbrpos += 4;
+                b = BitConverter.GetBytes(BlockNum[i]);
+                SDFS.UtilityMethods.CopyByteToByte(b, 0, data, (int)mbrpos, b.Length);
+                mbrpos += 4;
+                Device.WriteBlock(StartBlock[i], 1, Device.NewBlockArray(1));
+                Console.WriteLine("Partition N. " + (i + 1) + " Start: " + (StartBlock[i]).ToString() + " BlockCount: " + (BlockNum[i]).ToString());
+            }
+            Device.WriteBlock(0, 1, data);
+        }
+    }
+}
--- source/SDFS_Demo/Kernel.cs
+++ source/SDFS_Demo/Kernel.cs
@@ -4,22 +4,60 @@ using System.Text;
 using Sys = Cosmos.System;
 using SDFS;
 using SDFS.Logical;
+using SDFS.Physical;
 
 namespace SDFS_Demo
 {
     public class Kernel : Sys.Kernel
     {
+        /// <summary>
+        /// Partition containing a valid SDFS partition
+        /// </summary>
+        private static PrimaryPartition workPartition = null;
+
         protected override void BeforeRun()
         {
-            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            IDE[] IDEs = IDE.Devices.ToArray();
+            Console.WriteLine("Number of IDE disks: " + IDEs.Length);
+            Console.WriteLine("Looking for valid partitions...");
+            for (int i = 0; i < IDEs.Length; i++)
+            {
+                PrimaryPartition[] parts = IDEs[i].PrimaryPartitions;
+                for (int j = 0; j < parts.Length; j++)
+                {
+                    if (parts[j].Info.SystemID == 0xFA)
+                    {
+                        workPartition = parts[j];
+                    }
+                }
+            }
+            //#warning Revert to == null!!!
+            if (workPartition == null)
+            {
+                DiskHandler.CreatePartitions(IDEs);
+                Console.WriteLine("The machine needs to be restarted.");
+                Console.ReadKey(true);
+                Sys.Power.Reboot();
+            }
+
+            Console.Write("Checking FileSystem... ");
+            Filesystem fs;
+            try
+            {
+                fs = new Filesystem(workPartition);
+                Filesystem.MapFilesystem(fs);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!" + ex.Message);
+            }
         }
 
         protected override void Run()
         {
-            Console.Write("Input: ");
-            var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+            Console.WriteLine(workPartition.Info.TotalSize + "MBs");
+            Console.WriteLine("Type: " + workPartition.Info.SystemID.ToString());
+            Console.ReadKey(true);
         }
     }
 }
